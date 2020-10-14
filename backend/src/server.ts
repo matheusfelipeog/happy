abd2bb544101d539ef5b144b949c05e8c9b3268d
Matheus Felipe <50463866+matheusfelipeog@users.noreply.github.com>
2020-10-14 16:34:34 -0300

    Implemented views of orphanage

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,4 +1,5 @@
 import express from 'express';
+import path from 'path';
 
 import './database/connection';
 import routes from './routes';
@@ -7,5 +8,6 @@ const app = express();
 
 app.use(express.json());
 app.use(routes);
+app.use('/uploads', express.static(path.join(__dirname, '..', 'uploads')))
 
 app.listen(3333);
