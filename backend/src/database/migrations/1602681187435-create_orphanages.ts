0291042865e81c75c45fed6118b493bf6951cde1
Matheus Felipe <50463866+matheusfelipeog@users.noreply.github.com>
2020-10-14 10:55:09 -0300

    Redefine model of migrations for database

--- backend/src/database/migrations/1602681187435-create_orphanages.ts
+++ backend/src/database/migrations/1602681187435-create_orphanages.ts
@@ -39,6 +39,10 @@ export class createOrphanages1602681187435 implements MigrationInterface {
           type: "text"
         },
         {
+          name: "opening_hours",
+          type: "varchar"
+        },
+        {
           name: "open_on_weekends",
           type: "boolean",
           default: false
@@ -50,5 +54,4 @@ export class createOrphanages1602681187435 implements MigrationInterface {
   public async down(queryRunner: QueryRunner): Promise<void> {
     await queryRunner.dropTable("orphanages");
   }
-
 }
