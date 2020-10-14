17bf8684ed624c3347827acc7087e17e64b00cd4
Matheus Felipe <50463866+matheusfelipeog@users.noreply.github.com>
2020-10-14 12:40:32 -0300

    Create orphanage listing and show orphanage

--- backend/src/controllers/OrphanagesController.ts
+++ backend/src/controllers/OrphanagesController.ts
@@ -4,6 +4,25 @@ import { getRepository } from 'typeorm';
 import Orphanage from '../models/Orphanage';
 
 export default {
+  async index(req: Request, res: Response) {
+    const orphanagesRepository = getRepository(Orphanage);
+
+    const orphanages = await orphanagesRepository.find();
+
+    return res.json(orphanages);
+  },
+
+  async show(req: Request, res: Response) {
+
+    const { id } = req.params;
+
+    const orphanagesRepository = getRepository(Orphanage);
+
+    const orphanage = await orphanagesRepository.findOneOrFail(id);
+
+    return res.json(orphanage);
+  },
+
   async create(req: Request, res: Response) {
     const {
       name,
@@ -14,9 +33,9 @@ export default {
       opening_hours,
       open_on_weekends
     } = req.body;
-  
+
     const orphanagesRepository = getRepository(Orphanage);
-  
+
     const orphanage = orphanagesRepository.create({
       name,
       latitude,
@@ -26,9 +45,9 @@ export default {
       opening_hours,
       open_on_weekends
     });
-  
+
     await orphanagesRepository.save(orphanage);
-  
+
     res.status(201).json(orphanage);
   }
 }
--- backend/src/routes.ts
+++ backend/src/routes.ts
@@ -4,6 +4,8 @@ import OrphanagesController, {} from './controllers/OrphanagesController';
 
 const routes = Router();
 
+routes.get('/orphanages', OrphanagesController.index);
+routes.get('/orphanages/:id', OrphanagesController.show);
 routes.post('/orphanages', OrphanagesController.create);
 
 export default routes;
