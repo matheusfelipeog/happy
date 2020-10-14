57ec8b2a15798df933e3afab4ff2bb8023f4cc4b
Matheus Felipe <50463866+matheusfelipeog@users.noreply.github.com>
2020-10-14 17:00:15 -0300

    Implemented error handling

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,13 +1,18 @@
 import express from 'express';
+import 'express-async-errors';
+
 import path from 'path';
 
 import './database/connection';
+
 import routes from './routes';
+import errorHandler from './errors/handler';
 
 const app = express();
 
 app.use(express.json());
 app.use(routes);
 app.use('/uploads', express.static(path.join(__dirname, '..', 'uploads')))
+app.use(errorHandler);
 
 app.listen(3333);
