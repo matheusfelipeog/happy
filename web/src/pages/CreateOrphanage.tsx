8f9d8bd85dd35a72bcd2d2e6b3f64dd585892b35
Matheus Felipe <50463866+matheusfelipeog@users.noreply.github.com>
2020-10-16 21:38:45 -0300

    Coordinate selected by clicking on the map

--- web/src/pages/CreateOrphanage.tsx
+++ web/src/pages/CreateOrphanage.tsx
@@ -1,6 +1,7 @@
-import React from "react";
+import React, { useState } from "react";
 import { Map, Marker, TileLayer } from 'react-leaflet';
 import { FiPlus } from "react-icons/fi";
+import { LeafletMouseEvent } from 'leaflet';
 
 import Sidebar from "../components/SideBar";
 import mapIcon from "../utils/mapIcon";
@@ -8,6 +9,17 @@ import mapIcon from "../utils/mapIcon";
 import '../styles/pages/create-orphanage.css';
 
 export default function CreateOrphanage() {
+  const [position, setPosition] = useState({ latitude: 0, longitude: 0 });
+
+  function handleMapClick(event: LeafletMouseEvent) {
+    const { lat, lng } = event.latlng;
+
+    setPosition({
+      latitude: lat,
+      longitude: lng
+    });
+  }
+
   return (
     <div id="page-create-orphanage">
       <Sidebar />
@@ -17,16 +29,27 @@ export default function CreateOrphanage() {
           <fieldset>
             <legend>Dados</legend>
 
-            <Map 
-              center={[-27.2092052,-49.6401092]} 
+            <Map
+              center={[-27.2092052, -49.6401092]}
               style={{ width: '100%', height: 280 }}
               zoom={15}
+              onclick={handleMapClick}
             >
-              <TileLayer 
+              <TileLayer
                 url={`https://api.mapbox.com/styles/v1/mapbox/light-v10/tiles/256/{z}/{x}/{y}@2x?access_token=${process.env.REACT_APP_MAPBOX_TOKEN}`}
               />
 
-              <Marker interactive={false} icon={mapIcon} position={[-27.2092052,-49.6401092]} />
+              {position.latitude !== 0 && (
+                <Marker
+                  interactive={false}
+                  icon={mapIcon}
+                  position={[
+                    position.latitude,
+                    position.longitude
+                  ]}
+                />
+              )}
+
             </Map>
 
             <div className="input-block">
